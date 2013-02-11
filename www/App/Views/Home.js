1c26fac6f1b159610e05f9128bf056af0d65bda3
Eddie <eddie_D_2000@hotmail.com>
2013-02-11 16:56:27 -0600

    Getting the homepage in

--- www/App/Views/Home.js
+++ www/App/Views/Home.js
@@ -3,10 +3,15 @@ define([
         'underscore',
         'backbone',
         'Lib/Require/Plugins/text!Templates/Home.html'
-    ], function ($, _, backbone, marionette, templ) {
+    ], function ($, _, backbone, templ) {
         var view = backbone.View.extend({            
             template: templ,
             initialize: function () {
+            },
+            render: function () {
+                var template = _.template(templ);
+                this.$el.html( template );
+                return this.el;
             }
         });
 
--- www/App/Views/MainView.js
+++ www/App/Views/MainView.js
@@ -5,15 +5,18 @@
         'marionette',
         'Views/Home',
         'Lib/Require/Plugins/text!Templates/Main.html'
-    ], function ($, _, backbone, marionette, homeView, mainViewTemplate) {
+    ], function ($, _, backbone, marionette, view, mainViewTemplate) {
         var layout = marionette.Layout.extend({            
             template: mainViewTemplate,
             regions: {                
                 content: "#content"
             },
             initialize: function () {
-                this.layout = new homeView();
-                this.content.show(this.layout);
+                
+            },
+            onShow: function (){
+                var homeView = new view();
+                this.content.show(homeView);
             }
         });
 
