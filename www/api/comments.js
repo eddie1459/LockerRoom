37a13a9e407a31af7404c04b5790fd679acf8f24
Corey Perkins <corey.perkins@gmail.com>
2013-02-11 16:59:48 -0600

    Made the POSTs for Comments and Topics save
    There is still some socket.io work to do here as well as some error
    reporting and logging...

--- www/api/comments.js
+++ www/api/comments.js
@@ -2,8 +2,8 @@ function make(app, model, io) {
 	app.get('/api/comment', function(req, res) {
       return model.find({ 
       	  topicid: req.params.topicid,
-      	  count: req.params.count,		// get the last 20?
-      	  since: req.params.since		// get since?
+      	  count: req.params.count,		// get the last 20? this is also probably not suppose to be in the find
+      	  since: req.params.since		// get since? this is also probably not suppose to be in the find
         }, function(err, comments) {
 	    if (!err) {
 			res.contentType('application/json');
@@ -18,10 +18,27 @@ function make(app, model, io) {
 	});
 
 	app.post('/api/comment', function(req, res) {	  
-	  // TODO:  Implement persistence
-	  //var n = addComment(model, io, req.body);	  
-	  //return res.send(n);
+	  var c = addComment(model, io, req.body);	  
+	  return res.send(c);
+	});
+}
+
+function addComment(model, io, data) {
+	console.log("Topic posted: ");
+	console.log(data);
+
+	var c = new model({
+		topicid: data.topicid,
+		conent: data.content
+	});
+	c.save(function(err) {
+		if (!err) {
+			// TODO:  This is where socket.io comes in. We've saved the record so now we need to tell everyone!	
+		} else {
+			// TODO:  Report problem back to the user...
+		}
 	});
 }
 
 module.exports.make = make;
+module.exports.addComment = addComment;
--- www/api/topics.js
+++ www/api/topics.js
@@ -3,13 +3,14 @@ function make(app, model, io) {
       return model.find({ 
       	  sportid: req.params.sportid,
       	  teamid: req.params.teamid,
-      	  count: req.params.count,		// get the last 20?
-      	  since: req.params.since		// get since?
+      	  count: req.params.count,		// get the last 20? this is also probably not suppose to be in the find
+      	  since: req.params.since		// get since? this is also probably not suppose to be in the find
         }, function(err, topics) {
 	    if (!err) {
 			res.contentType('application/json');
 			console.log(req.params);
 			
+			// TODO:  Order by timestamp
 			return res.send(topics);
 	    } else {
 	      return console.log(err);
@@ -18,10 +19,28 @@ function make(app, model, io) {
 	});
 
 	app.post('/api/topic', function(req, res) {	  
-	  // TODO:  Implement persistence
-	  //var n = addTopic(model, io, req.body);	  
-	  //return res.send(n);
+	  var t = addTopic(model, io, req.body);	  
+	  return res.send(t);
+	});
+}
+
+function addTopic(model, io, data) {
+	console.log("Topic posted: ");
+	console.log(data);
+
+	var t = new model({
+		sportid: data.sportid,
+		teamid: data.teamid,
+		name: data.name
+	});
+	t.save(function(err) {
+		if (!err) {
+			// TODO:  This is where socket.io comes in. We've saved the record so now we need to tell everyone!	
+		} else {
+			// TODO:  Report problem back to the user...
+		}
 	});
 }
 
 module.exports.make = make;
+module.exports.addTopic = addTopic;
