b571d5a488f69d74e258a563f2f7e41d9a3a0507
Eddie Dillon <eddie.d.2000@gmail.com>
2013-02-12 15:35:56 -0600

    Adding in the Messages View and hooking it to the topics

--- www/App/Views/Messages.js
+++ www/App/Views/Messages.js
@@ -2,25 +2,17 @@ define([
         'jquery',
         'underscore',
         'backbone',
-	'Views/Messages'
-        'Lib/Require/Plugins/text!Templates/Topics.html'
+        'Views/Messages',
+        'Lib/Require/Plugins/text!Templates/Messages.html'
     ], function ($, _, backbone, msgsView, templ) {
         var view = backbone.View.extend({            
             template: templ,
-	    events: {
-                "click li": "showMessages"
-            },
             initialize: function () {
             },
             render: function () {
                 var template = _.template(templ);
                 this.$el.html( template );
                 return this.el;
-            },
-            showMessages: function (evt) {
-                //TODO: need to show messages by topicId
-                var view = new msgsView();
-                LockerRoom.main.show(view);
             }
         });
 
