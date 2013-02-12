0b1e806406d519e60f564b6c7e836cfdce95e6f9
Corey Perkins <corey.perkins@gmail.com>
2013-02-11 21:46:53 -0600

    Fixed a conflict

--- www/App/Main.js
+++ www/App/Main.js
@@ -1,5 +1,4 @@
 ﻿require.config({
-<<<<<<< HEAD
     // baseUrl: '/www/App',
     // baseUrl: '/App'
 //               ,
@@ -29,35 +28,6 @@
 //            exports: "cordova"
 //        },
 //    }
-=======
-    baseUrl: '/App',
-    paths: {
-        "jquery": 'Lib/Jquery/jquery-min',
-        "underscore": 'Lib/Underscore/underscore-min',
-        "backbone": 'Lib/Backbone/backbone-min',
-        "marionette": 'Lib/Backbone/backbone.marionette',
-        "iscroll": 'Lib/iscroll/iscroll-lite'
-        //"cordova": 'Lib/cordova-2.3.0'
-    },
-    shims: {
-        "jquery": {
-            exports: ["jQuery", "$"]
-        },
-        "backbone": {
-            deps: ["underscore", "jquery"],
-            exports: "Backbone"
-        },
-        "underscore": {
-            deps: ["jquery"]
-        },
-        "marionette": {
-            deps: ["jquery", "underscore", "backbone"]
-        }
-        //"cordova": {
-        //    exports: "cordova"
-        //},
-    }
->>>>>>> messing with the js to fix phonegap
 });
 
 require([
