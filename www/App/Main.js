a938bc6c91686ddceaf94bebea3bb679dcadfaee
Eddie <eddie_D_2000@hotmail.com>
2013-02-11 21:41:05 -0600

    messing with the js to fix phonegap

--- www/App/Main.js
+++ www/App/Main.js
@@ -1,4 +1,5 @@
 ﻿require.config({
+<<<<<<< HEAD
     // baseUrl: '/www/App',
     // baseUrl: '/App'
 //               ,
@@ -28,6 +29,35 @@
 //            exports: "cordova"
 //        },
 //    }
+=======
+    baseUrl: '/App',
+    paths: {
+        "jquery": 'Lib/Jquery/jquery-min',
+        "underscore": 'Lib/Underscore/underscore-min',
+        "backbone": 'Lib/Backbone/backbone-min',
+        "marionette": 'Lib/Backbone/backbone.marionette',
+        "iscroll": 'Lib/iscroll/iscroll-lite'
+        //"cordova": 'Lib/cordova-2.3.0'
+    },
+    shims: {
+        "jquery": {
+            exports: ["jQuery", "$"]
+        },
+        "backbone": {
+            deps: ["underscore", "jquery"],
+            exports: "Backbone"
+        },
+        "underscore": {
+            deps: ["jquery"]
+        },
+        "marionette": {
+            deps: ["jquery", "underscore", "backbone"]
+        }
+        //"cordova": {
+        //    exports: "cordova"
+        //},
+    }
+>>>>>>> messing with the js to fix phonegap
 });
 
 require([
