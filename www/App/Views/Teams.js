85ef924d443d2f632cd5d0cf686ac80d20913385
Eddie <eddie_D_2000@hotmail.com>
2013-02-11 23:56:07 -0600

    Adding in a hard coded teams page

--- www/App/Views/Teams.js
+++ www/App/Views/Teams.js
@@ -2,24 +2,16 @@ define([
         'jquery',
         'underscore',
         'backbone',
-        'Views/Teams',
-        'Lib/Require/Plugins/text!Templates/Home.html'
-    ], function ($, _, backbone, teamsView, templ) {
+        'Lib/Require/Plugins/text!Templates/Teams.html'
+    ], function ($, _, backbone, templ) {
         var view = backbone.View.extend({            
             template: templ,
-            events: {
-                "click #showTeams": "showTeams"
-            },
             initialize: function () {
             },
             render: function () {
                 var template = _.template(templ);
                 this.$el.html( template );
                 return this.el;
-            },
-            showTeams: function (){
-                var view = new teamsView();
-                LockerRoom.main.show(view);
             }
         });
 
