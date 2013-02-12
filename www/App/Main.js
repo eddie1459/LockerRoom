2a035ca7819da77dde51306f9f508261944bbf2b
Corey Perkins <corey.perkins@gmail.com>
2013-02-11 22:01:47 -0600

    Fixed the require glitch...the UI is now loading...

--- www/App/Main.js
+++ www/App/Main.js
@@ -1,38 +1,36 @@
 ﻿require.config({
-    // baseUrl: '/www/App',
-    // baseUrl: '/App'
-//               ,
-//    paths: {
-//        "jquery": 'Lib/Jquery/jquery-min',
-//        "underscore": 'Lib/Underscore/underscore-min',
-//        "backbone": 'Lib/Backbone/backbone-min',
-//        "marionette": 'Lib/Backbone/backbone.marionette',
-//        "iscroll": 'Lib/iscroll/iscroll-lite',
-//        "cordova": 'Lib/cordova-2.3.0'
-//    },
-//    shims: {
-//        "jquery": {
-//            exports: ["jQuery", "$"]
-//        },
-//        "backbone": {
-//            deps: ["underscore", "jquery"],
-//            exports: "Backbone"
-//        },
-//        "underscore": {
-//            deps: ["jquery"]
-//        },
-//        "marionette": {
-//            deps: ["jquery", "underscore", "backbone"]
-//        },
-//        "cordova": {
-//            exports: "cordova"
-//        },
-//    }
+    paths: {
+        "jquery": 'Lib/Jquery/jquery-min',
+        "underscore": 'Lib/Underscore/underscore-min',
+        "backbone": 'Lib/Backbone/backbone-min',
+        "marionette": 'Lib/Backbone/backbone.marionette',
+        "iscroll": 'Lib/iscroll/iscroll-lite',
+        "cordova": 'Lib/cordova-2.3.0'
+    },
+    shims: {
+        "jquery": {
+            exports: ["jQuery", "$"]
+        },
+        "backbone": {
+            deps: ["underscore", "jquery"],
+            exports: "Backbone"
+        },
+        "underscore": {
+            deps: ["jquery"]
+        },
+        "marionette": {
+            deps: ["jquery", "underscore", "backbone"]
+        },
+        "cordova": {
+            exports: "cordova"
+        },
+    }
 });
 
 require([
     'app'
 ], function (app) {
-    alert(app);
-    alert("Fuck You From Require.JS");
+        app.initialize();
+    // alert(app);
+    // alert("Fuck You From Require.JS");
 });
