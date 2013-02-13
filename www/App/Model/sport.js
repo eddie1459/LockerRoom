fbdc5ec89031fb8789b06101460d4e89489b398f
Corey Perkins <corey.perkins@gmail.com>
2013-02-13 11:35:54 -0600

    Added a couple models...

---
+++ www/App/Model/sport.js
@@ -0,0 +1,14 @@
+define([
+  'underscore',
+  'backbone'
+], function (_, backbone) {
+    return backbone.Model.extend({
+        url: '/api/sport,
+        defaults: function () {
+            return {
+                name: '',
+                timestamp: ''
+            };
+        }
+    });
+});
---
+++ www/App/Model/state.js
@@ -0,0 +1,16 @@
+define([
+  'underscore',
+  'backbone'
+], function (_, backbone) {
+    return backbone.Model.extend({
+        url: '/api/state,
+        defaults: function () {
+            return {
+                name: '',
+                abbreviation: '',
+                timestamp: '',
+                counties: []
+            };
+        }
+    });
+});
