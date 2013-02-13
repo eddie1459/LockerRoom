5f13a62bd54e0803b510386bfb66dd558a7ae953
Corey Perkins <corey.perkins@gmail.com>
2013-02-12 18:51:06 -0600

    Got the curl call for teams working...
    I also added some logging to the teams.js api

--- www/api/teams.js
+++ www/api/teams.js
@@ -1,13 +1,20 @@
 function make(app, model, io) {
 	app.get('/api/team', function(req, res) {
+	  var stateid = req.query["stateid"];
+      var sportid = req.query["sportid"];
+
+      console.log("Request url: " + req.url);
+      console.log("stateid: " + stateid);
+      console.log("sportid: " + sportid);
+
       return model.find({ 
-      	  stateid: req.params.stateid,
-      	  sportid: req.params.sportid
+      	  stateid: stateid,
+      	  sportid: sportid
         }, function(err, teams) {
 	    if (!err) {
 			res.contentType('application/json');
 			console.log(req.params);
-			
+			console.log(teams);
 			return res.send(teams);
 	    } else {
 	      return console.log(err);
