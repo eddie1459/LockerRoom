b13b6dbbae2c9330302c8a14ddef2b9ed7427461
Corey Perkins <corey.perkins@gmail.com>
2013-02-24 11:32:59 -0600

    Updated jq mobile to 1.3

--- www/App/Main.js
+++ www/App/Main.js
@@ -1,7 +1,7 @@
 ﻿require.config({
     paths: {
         "jquery": 'http://code.jquery.com/jquery-1.8.2.min',
-        "jquerymobile": "http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min",
+        "jquerymobile": "http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min",
         "underscore": 'Lib/Underscore/underscore-min',
         "backbone": 'Lib/Backbone/backbone-min',
         "marionette": 'Lib/Backbone/backbone.marionette',
