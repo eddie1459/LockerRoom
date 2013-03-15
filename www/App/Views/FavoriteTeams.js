720e836b241186a92f0a9c284db4234518f45e6c
Eddie <eddie_D_2000@hotmail.com>
2013-03-14 22:31:04 -0500

    Changed the FavoriteTeams to have delete icons, some polishing too

--- www/App/Views/FavoriteTeams.js
+++ www/App/Views/FavoriteTeams.js
@@ -6,7 +6,10 @@ define([
         'Lib/Require/Plugins/text!Templates/FavoriteTeams.html'
     ], function ($, _, backbone, marionette, templ) {
         var view = marionette.ItemView.extend({            
-            template: templ
+            template: templ,
+            onShow: function() {
+                $('#wrapper').trigger('create');
+            }
         });
 
         return view;
