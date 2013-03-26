55eb42a9475646343d0e726e199e8d5ed921e6d0
Corey Perkins <corey.perkins@gmail.com>
2013-03-25 21:34:01 -0500

    Modified /api/user to get the id from the request

--- www/api/users.js
+++ www/api/users.js
@@ -1,7 +1,7 @@
 function make(app, model, io) {
-	app.get('/api/user/:id', function(req, res) {
-	  // TODO:  This should be able to handle getting the user id itself...somehow...maybe a separate api call???
-	  return model.user.findOne(id, function (err, user) {
+	app.get('/api/user', function(req, res) {
+	  var id = req.user._id;
+	  return model.findOne(id, function (err, user) {
 	  	if (!err) {
           return res.send(user);
 	  	} else {
